03bd96fbf75c697b9c0e23bd625b64b6a2fe989b
Nils Yuan <Nilsyuan@gmail.com>
2018-09-07 16:01:51 -0400

    I completed number 3.

---
+++ exercise3.rb
@@ -0,0 +1,12 @@
+def is_even?(my_number)
+  if my_number % 2 == 0
+    return true
+  else
+    return false
+  end
+end
+
+puts is_even?(2)
+puts is_even?(33)
+puts is_even?(47)
+puts is_even?(100)
